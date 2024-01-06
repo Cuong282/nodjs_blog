817f24c52dad9854bf68ca89177d15d150dc3fc9
cuong <ntcuong2822002@gmail.com>
2024-01-06 15:53:25 +0700

    123

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
-const port = 3000
+const port = 3001
+
+const route =require ('./routes')
 
 app.use(express.static(path.join(__dirname,'public')));
 
 //template engine
+
 app.engine('hbs', handlebars.engine({
   extname:'.hbs'
 }));
@@ -16,15 +19,8 @@ app.set('view engine','hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log(path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-app.get('/search', (req, res) => {
-  res.render('search')
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
